a6f48fc53efb308e748a3efa27b2db73c327dd44
Alex Ballas <alex@ballas.org>
2022-04-24 23:00:08 +0300

    reduce cyclomatic complexities

--- httphandlers/httphandlers.go
+++ httphandlers/httphandlers.go
@@ -193,100 +193,102 @@ func serveContent(w http.ResponseWriter, r *http.Request, tv *soapcalls.TVPayloa
 		mediaType = tv.MediaType
 	}
 
-	respHeader := w.Header()
-
-	// No need to set "Transfer-Encoding: chunked" as
-	// the std library seems to explicitely add it.
-
-	respHeader["transferMode.dlna.org"] = []string{"Interactive"}
+	w.Header()["transferMode.dlna.org"] = []string{"Interactive"}
 
 	if isMedia {
-		respHeader["transferMode.dlna.org"] = []string{"Streaming"}
-		respHeader["realTimeInfo.dlna.org"] = []string{"DLNA.ORG_TLAG=*"}
+		w.Header()["transferMode.dlna.org"] = []string{"Streaming"}
+		w.Header()["realTimeInfo.dlna.org"] = []string{"DLNA.ORG_TLAG=*"}
 	}
 
 	switch f := mf.(type) {
 	case osFileType:
-		if r.Header.Get("getcontentFeatures.dlna.org") == "1" {
-
-			// comments
-			seek := "01"
-			if strings.Contains(mediaType, "video") && transcode {
-				seek = "00"
-			}
-
-			contentFeatures, err := utils.BuildContentFeatures(mediaType, seek, transcode)
-			if err != nil {
-				http.NotFound(w, r)
-				return
-			}
-
-			respHeader["contentFeatures.dlna.org"] = []string{contentFeatures}
-		}
-
-		// Since we're dealing with an io.Reader we can't
-		// allow any HEAD requests that some DMRs trigger.
-		if transcode && r.Method == http.MethodGet && strings.Contains(mediaType, "video") {
-			var input interface{} = f.file
-			// The only case where we should expect f.path to be ""
-			// is only during our unit tests where we emulate the files.
-			if f.path != "" {
-				input = f.path
-			}
-			_ = utils.ServeTranscodedStream(w, input, ff)
-			return
-		}
-
-		name := strings.TrimLeft(r.URL.Path, "/")
-
-		if r.Method == http.MethodGet {
-			http.ServeContent(w, r, name, f.time, f.file)
-		}
-
+		serveContentCustomType(r, mediaType, transcode, w, f, ff)
 	case []byte:
-		if r.Header.Get("getcontentFeatures.dlna.org") == "1" {
-			contentFeatures, err := utils.BuildContentFeatures(mediaType, "01", false)
-			if err != nil {
-				http.NotFound(w, r)
-				return
-			}
-
-			respHeader["contentFeatures.dlna.org"] = []string{contentFeatures}
-		}
-
-		bReader := bytes.NewReader(f)
-
-		name := strings.TrimLeft(r.URL.Path, "/")
-		http.ServeContent(w, r, name, time.Now(), bReader)
-
+		serveContentBytes(r, mediaType, w, f)
 	case io.ReadCloser:
-		if r.Header.Get("getcontentFeatures.dlna.org") == "1" {
-			contentFeatures, err := utils.BuildContentFeatures(mediaType, "00", transcode)
-			if err != nil {
-				fmt.Println(err)
-				http.NotFound(w, r)
-				return
-			}
-
-			respHeader["contentFeatures.dlna.org"] = []string{contentFeatures}
-		}
-
-		// Since we're dealing with an io.Reader we can't
-		// allow any HEAD requests that some DMRs trigger.
-		if transcode && r.Method == http.MethodGet && strings.Contains(mediaType, "video") {
-			_ = utils.ServeTranscodedStream(w, f, ff)
-			return
-		}
-
-		// No seek support
-		if r.Method == http.MethodGet {
-			_, _ = io.Copy(w, f)
-			f.Close()
-			return
-		}
-
+		serveContentReadClose(r, mediaType, transcode, w, f, ff)
 	default:
 		http.NotFound(w, r)
 		return
+	}
+}
+
+func serveContentBytes(r *http.Request, mediaType string, w http.ResponseWriter, f []byte) {
+	if r.Header.Get("getcontentFeatures.dlna.org") == "1" {
+		contentFeatures, err := utils.BuildContentFeatures(mediaType, "01", false)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		w.Header()["contentFeatures.dlna.org"] = []string{contentFeatures}
+	}
+
+	bReader := bytes.NewReader(f)
+	name := strings.TrimLeft(r.URL.Path, "/")
+	http.ServeContent(w, r, name, time.Now(), bReader)
+}
+
+func serveContentReadClose(r *http.Request, mediaType string, transcode bool, w http.ResponseWriter, f io.ReadCloser, ff *exec.Cmd) {
+	if r.Header.Get("getcontentFeatures.dlna.org") == "1" {
+		contentFeatures, err := utils.BuildContentFeatures(mediaType, "00", transcode)
+		if err != nil {
+			fmt.Println(err)
+			http.NotFound(w, r)
+			return
+		}
+
+		w.Header()["contentFeatures.dlna.org"] = []string{contentFeatures}
+	}
+
+	// Since we're dealing with an io.Reader we can't
+	// allow any HEAD requests that some DMRs trigger.
+	if transcode && r.Method == http.MethodGet && strings.Contains(mediaType, "video") {
+		_ = utils.ServeTranscodedStream(w, f, ff)
+		return
+	}
+
+	// No seek support
+	if r.Method == http.MethodGet {
+		_, _ = io.Copy(w, f)
+		f.Close()
+		return
+	}
+}
+
+func serveContentCustomType(r *http.Request, mediaType string, transcode bool, w http.ResponseWriter, f osFileType, ff *exec.Cmd) {
+	if r.Header.Get("getcontentFeatures.dlna.org") == "1" {
+
+		seek := "01"
+		if strings.Contains(mediaType, "video") && transcode {
+			seek = "00"
+		}
+
+		contentFeatures, err := utils.BuildContentFeatures(mediaType, seek, transcode)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		w.Header()["contentFeatures.dlna.org"] = []string{contentFeatures}
+	}
+
+	if transcode && r.Method == http.MethodGet && strings.Contains(mediaType, "video") {
+		// Since we're dealing with an io.Reader we can't
+		// allow any HEAD requests that some DMRs trigger.
+		var input interface{} = f.file
+		// The only case where we should expect f.path to be ""
+		// is only during our unit tests where we emulate the files.
+		if f.path != "" {
+			input = f.path
+		}
+		_ = utils.ServeTranscodedStream(w, input, ff)
+		return
+	}
+
+	name := strings.TrimLeft(r.URL.Path, "/")
+
+	if r.Method == http.MethodGet {
+		http.ServeContent(w, r, name, f.time, f.file)
 	}
 }
