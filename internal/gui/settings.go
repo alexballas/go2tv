4f55b817183f11667d95b862026cb9a542b69b33
Alex Ballas <alex@ballas.org>
2022-12-27 02:08:51 +0200

    Update theme

--- internal/gui/settings.go
+++ internal/gui/settings.go
@@ -20,8 +20,31 @@ func (m go2tvTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) c
 	switch m.Theme {
 	case "Dark":
 		variant = theme.VariantDark
+		switch name {
+		case theme.ColorNameDisabled:
+			return color.NRGBA{R: 0x55, G: 0x55, B: 0x55, A: 0xff}
+		case theme.ColorNameBackground:
+			return color.NRGBA{R: 0x30, G: 0x30, B: 0x30, A: 0xff}
+		case theme.ColorNameButton:
+			return color.NRGBA{R: 0x44, G: 0x44, B: 0x44, A: 0xff}
+		case theme.ColorNameDisabledButton:
+			return color.NRGBA{R: 0x26, G: 0x26, B: 0x26, A: 0xff}
+		case theme.ColorNameOverlayBackground:
+			return color.NRGBA{R: 0x30, G: 0x30, B: 0x30, A: 0xff}
+		case theme.ColorNameMenuBackground:
+			return color.NRGBA{R: 0x30, G: 0x30, B: 0x30, A: 0xff}
+		}
+
 	case "Light":
 		variant = theme.VariantLight
+		switch name {
+		case theme.ColorNameDisabled:
+			return color.NRGBA{R: 0xab, G: 0xab, B: 0xab, A: 0xff}
+		case theme.ColorNameInputBorder:
+			return color.NRGBA{R: 0xf3, G: 0xf3, B: 0xf3, A: 0xff}
+		case theme.ColorNameDisabledButton:
+			return color.NRGBA{R: 0xe5, G: 0xe5, B: 0xe5, A: 0xff}
+		}
 	}
 
 	return theme.DefaultTheme().Color(name, variant)
