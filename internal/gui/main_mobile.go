61454c51f505a9551b8b74f23f6a492cfb9627b3
Alex Ballas <alex@ballas.org>
2023-05-23 19:55:14 +0300

    Fix Android build

--- internal/gui/main_mobile.go
+++ internal/gui/main_mobile.go
@@ -4,6 +4,7 @@
 package gui
 
 import (
+	"context"
 	"errors"
 	"net/url"
 	"sort"
@@ -141,7 +142,7 @@ func mainWindow(s *NewScreen) fyne.CanvasObject {
 	// Widgets actions
 	list.OnSelected = func(id widget.ListItemID) {
 		playpause.Enable()
-		t, err := soapcalls.DMRextractor(data[id].addr)
+		t, err := soapcalls.DMRextractor(context.Background(), data[id].addr)
 		check(w, err)
 		if err == nil {
 			s.selectedDevice = data[id]
