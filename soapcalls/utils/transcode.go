4c170618a537e14b824153b1de8c3a8dd225061b
Alex Ballas <alex@ballas.org>
2025-07-05 01:03:02 +0300

    Remove padding

--- soapcalls/utils/transcode.go
+++ soapcalls/utils/transcode.go
@@ -44,7 +44,7 @@ func ServeTranscodedStream(w io.Writer, input any, ff *exec.Cmd, ffmpegPath, sub
 		}
 	}
 
-	vf = "scale='min(1920,iw)':min'(1080,ih)':force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2," + vf
+	vf = "scale='min(1920,iw)':min'(1080,ih)':force_original_aspect_ratio=decrease," + vf
 
 	cmd := exec.Command(
 		ffmpegPath,
